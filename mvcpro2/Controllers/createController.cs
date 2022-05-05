5e759928943eed39f95c912d1f64f1f9a357be1d
abc <abc@DESKTOP-A7C0CTR>
2022-05-06 01:30:16 +0530

    Add project files.

---
+++ mvcpro2/Controllers/createController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcpro2.Controllers
+{
+    public class createController : Controller
+    {
+        // GET: create
+        public ActionResult Index()
+        {
+            string m = ".net mvc project";
+            ViewBag.msg = m;
+            return View();
+        }
+        public ActionResult home1()
+        {
+            string m = "chetu";
+            ViewBag.msg = m;
+            return View();
+        }
+        public ActionResult aboutus()
+        {
+            string m = "software development";
+            ViewBag.msg = m;
+            return View();
+        }
+    }
+}
